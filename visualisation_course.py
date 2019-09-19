c5663214978066636e9b4d8a6abb82f66d907e06
Aethelfrith <52699977+Aethelfrith@users.noreply.github.com>
2019-09-19 13:13:00 +0200

    Finish part one and start part two of the course

--- visualisation_course.py
+++ visualisation_course.py
@@ -2,4 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-fifa_filepath = "fifa.csv"
+
+#PART 1
+#Register datatime converters
+#from pandas.plotting import register_matplotlib_converters
+#register_matplotlib_converters()
+
+##Load data
+#fifa_filepath = "./Data/fifa.csv"
+#fifa_data = pd.read_csv(fifa_filepath, index_col ="Date", parse_dates = True)
+
+##Inspect first rows
+#print(fifa_data.head())
+
+#Make line chart
+#plt.figure(figsize=(16,6))
+#sns.lineplot(data=fifa_data)
+#plt.show()
+
+
+#PART 2
+#Read the file
+spotify_filename = "./Data/spotify.csv"
+spotify_data = pd.read_csv(spotify_filename, index_col="Date",parse_dates=True)
+
+#Inspect data
+print(spotify_data.head())
+
+
