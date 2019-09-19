12a282a4756307636f680fc5c49b249c48be6352
Aethelfrith <52699977+Aethelfrith@users.noreply.github.com>
2019-09-19 13:25:40 +0200

    Finish part 2 and add data files

--- visualisation_course.py
+++ visualisation_course.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#Register datatime converters
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
 #PART 1
-#Register datatime converters
-#from pandas.plotting import register_matplotlib_converters
-#register_matplotlib_converters()
 
 ##Load data
 #fifa_filepath = "./Data/fifa.csv"
@@ -28,5 +28,20 @@ spotify_data = pd.read_csv(spotify_filename, index_col="Date",parse_dates=True)
 
 #Inspect data
 print(spotify_data.head())
+print(spotify_data.tail())
+
+#Print names of columns
+list(spotify_data.columns)
+
+#Plot data
+plt.figure(figsize=(14,6))
+plt.title("Daily global streams of popular songs in 2017-2018")
+plt.xlabel('Date')
+sns.lineplot(data=spotify_data['Shape of You'],label='Shape of You')
+sns.lineplot(data =spotify_data['Despacito'],label='Descpacito')
+plt.show()
+
+
+
 
 
