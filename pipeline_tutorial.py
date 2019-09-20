24e9768d6f825a26ec713b432fd59cdff0db7f56
Aethelfrith <52699977+Aethelfrith@users.noreply.github.com>
2019-09-20 08:43:48 +0200

    Separate numerical and categorical column names

--- pipeline_tutorial.py
+++ pipeline_tutorial.py
@@ -3,6 +3,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import model_selection as ms
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
 
 #Load the data
 housing_filename = "./Data/melb_data.csv"
@@ -22,6 +29,10 @@ X_col = list(X.columns.values)
 #print(y_col)
 #print(X_col)
 
+#Extract numerical column NAMES
+numerical_cols = list(X.select_dtypes(include = ['number']).columns.values)
+categorical_cols = list(X.select_dtypes(include = ['object']).columns.values)
+
 #Separate the data into training and validations sets
 random_seed = 1234;
 X_train,X_valid,y_train,y_valid = ms.train_test_split(X,y,test_size = 0.25, random_state = random_seed)
@@ -30,4 +41,20 @@ X_train,X_valid,y_train,y_valid = ms.train_test_split(X,y,test_size = 0.25, rand
 print(X_train.head())
 print(y_train.head())
 
+#Create the pipeline
 
+#Preprocess numerical data
+numerical_transformer = SimpleImputer(strategy = 'constant')
+
+#Preprocess categorical data
+categorical_transformer = Pipeline(steps = [('imputer',SimpleImputer(strategy='most_frequent')),
+('onehot',OneHotEncoder(handle_unknown = 'ignore'))])
+
+#Bundle the transformers
+preprocessor = ColumnTransformer( transformers = [('num',numerical_transformer,numerical_cols), ('cat',categorical_transformer,categorical_cols)] )
+
+#Builld a model
+model = RandomForestRegressor(n_estimators = 100, random_state = random_seed)
+
+#Build pipeline
+my_pipeline = Pipeline(steps=[('preprocessor',preprocessor),('model',model)])
